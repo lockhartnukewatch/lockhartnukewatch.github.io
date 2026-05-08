ad91e210cda11a6b67c73f3f9f2064574b3be2fa
Lockhart Nuke Watch <contact@lockhartnuke.watch>
2026-05-07 22:21:59 -0500

    Publish
    Publish 20261505/26/26-121548

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "FOIA requests",
+        "excerpt":"Why were FOIA requests filed for the Groves site? There is no confirmation that the linked NRC documents on this site actually describe the same VIPR reactor Oklo is building at the Groves site in Lockhart, Texas. A search for “Groves”, “Lockhart” or “Texas” on the Nuclear Regulatory Commission (NRC)...","categories": [],
+        "tags": [],
+        "url": "/archive/00-foia-requests/",
+        "teaser": null
+      },{
+        "title": "What do we really know about the reactor?",
+        "excerpt":"The technical specifications of the nuclear reactor Oklo is building in Lockhart, are still unconfirmed and unknown at time of writing. The only information shared with the public so far are corporate press releases and what might be inferred from Atomic Alchemy’s past NRC documents. Everything below is based on...","categories": [],
+        "tags": [],
+        "url": "/archive/10-vipr-reactor/",
+        "teaser": null
+      },{
+        "title": "Radioactive waste: Tritium",
+        "excerpt":"Tritium is the heaviest isotope of hydrogen. Tritium is dangerous when inhaled or consumed in contaminated molecules such as water. The tritium atoms produce beta radiation which damage organisms from the inside, as the body absorbs these atoms. Tritium is of particular concern because of the long half-life: 12.2 years....","categories": [],
+        "tags": [],
+        "url": "/archive/20-waste-tritium/",
+        "teaser": null
+      },{
+        "title": "Radioactive waste: Plutonium",
+        "excerpt":"The following is based on the limited open source materials on Atomic Alchemy’s VIPR reactor from the cancelled Meitner-1 project. Section 4.5.2.1 of Chapter 4 – Reactor Description ATOMIC ALCHEMY INC. states: effective delayed neutron fraction of the core is predicted to decrease from 0.66% to 0.60% as U-235 is...","categories": [],
+        "tags": [],
+        "url": "/archive/21-waste-plutonium/",
+        "teaser": null
+      },{
+        "title": "Understanding significant reactor transients",
+        "excerpt":"Section 4.1 of the reactor description [https://www.nrc.gov/docs/ML2525/ML25255A207.pdf] describes a regularly occurring event in the regular operation of the reactor: The most significant reactor transient, apart from startup and shutdown, occurs with the deliberate insertion or removal of high-reactivity irradiation targets while the system is operational. These target movements are planned...","categories": [],
+        "tags": [],
+        "url": "/archive/50-significant-reactor-events/",
+        "teaser": null
+      },{
+        "title": "FOIA requests",
+        "excerpt":"Why were FOIA requests filed for the Groves site? A search for “Groves”, “Lockhart” or “Texas” on the Nuclear Regulatory Commission (NRC) document site [ADAMS] and Department of Energy (DOE) document sites does not yield any meaningful results. You can confirm this for yourself by browsing the NRC ADAMS link....","categories": [],
+        "tags": [],
+        "url": "/topics/00-foia-requests/",
+        "teaser": null
+      },{
+        "title": "Article: Did Anyone Ask Lockhart?",
+        "excerpt":"Lone Star Left recently covered Lockhart’s fast-tracked nuclear reactor situation in Did Anyone Ask Lockhart?.   Thank you Michelle for sharing our story!  ","categories": [],
+        "tags": [],
+        "url": "/updates/2026-05-08-did-anyone-ask/",
+        "teaser": null
+      },{
+        "title": "Oklo responds with a press release, raising more questions",
+        "excerpt":"On May 11, 2026, Oklo seems to have responded to the questions raised by Lockhart Nuke Watch, with a slick press release. They’ve added a Texas page. It’s very high level and general, and still leaves many questions unanswered. Still, it is at least the start of what is hopefully...","categories": [],
+        "tags": [],
+        "url": "/updates/2026-05-11-our-response-to-oklo/",
+        "teaser": null
+      },{
+        "title": "What is the Groves reactor? A dubious experiment?",
+        "excerpt":"Oklo released a Texas fact sheet in response to questions Lockhart Nuke Watch asked about the Preliminary Documented Safety Analysis (PDSA) for the Groves site Oklo is building in Lockhart. The most significant takeaway: Oklo confirms that Groves will not be a VIPR reactor. So the prior analysis on this...","categories": [],
+        "tags": [],
+        "url": "/updates/2026-05-25-oklo-latest-groves-press-release/",
+        "teaser": null
+      }]
